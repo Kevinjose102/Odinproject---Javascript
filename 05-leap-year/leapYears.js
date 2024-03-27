1d635ec7a93205487423f33f3554a9ab99f6d471
Kevinjose102 <kevinjose2505@gmail.com>
2024-03-27 06:47:15 +0530

    checking for leap year

---
+++ 05-leap-year/leapYears.js
@@ -0,0 +1,13 @@
+function leapYears(year){
+    if(year % 4 === 0 && year % 100 != 0){
+        return true;
+    }
+    else if(year % 400 == 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+module.exports = leapYears;
